c7689c0b8bfb6b7c4792e508ba306e13d295753c
PacoLorente <lo-rente@hotmail.com>
2025-07-16 20:04:51 +0200

    16/7/25. Hay que crear la rutina que pinta el logo del menú principal.

--- Sprites_e_indices.asm
+++ Sprites_e_indices.asm
@@ -6,7 +6,88 @@
 
 	org $8360
 
-; Disparo.
+;	LOGO del menú principal.
+
+; 	ASM source file created by SevenuP v1.20
+; 	SevenuP (C) Copyright 2002-2006 by Jaime Tejedor Gomez, aka Metalbrain
+
+;	GRAPHIC DATA:
+
+;	Pixel Size:      ( 24,  24)
+;
+;	(Puntero_de_impresion) ..... $4060
+
+Logo_nave:
+
+	DEFB	$00,$18,$00,$00,$24,$00,$00,$42
+	DEFB	$00,$00,$7E,$00,$00,$CF,$00,$00
+	DEFB	$BF,$00,$09,$7F,$90,$19,$C3,$98
+	DEFB	$39,$81,$9C,$3B,$81,$DC,$7B,$81
+	DEFB	$DE,$7A,$C3,$DE,$DA,$FF,$DF,$DB
+	DEFB	$7F,$DF,$DD,$3F,$BF,$CD,$BF,$BF
+	DEFB	$65,$9F,$BE,$66,$9F,$7E,$3E,$FF
+	DEFB	$7C,$3C,$FF,$3C,$19,$7E,$98,$01
+	DEFB	$BD,$80,$03,$DB,$C0,$07,$00,$E0
+
+;	Pixel Size:      ( 24,  16)
+;
+;	(Puntero_de_impresion) ..... $4083
+
+Logo_ma:
+
+	DEFB	$00,$00,$00,$00,$00,$01,$03,$03
+	DEFB	$0F,$07,$87,$1F,$0F,$9F,$9E,$1D
+	DEFB	$B9,$80,$79,$F1,$80,$71,$E1,$87
+	DEFB	$F3,$C1,$8F,$E3,$83,$0F,$E7,$83
+	DEFB	$0C,$C7,$03,$0E,$C2,$07,$0F,$C0
+	DEFB	$07,$03,$00,$02,$00,$00,$00,$00
+
+;	Pixel Size:      ( 24,  24)
+;
+;	(Puntero_de_impresion) ..... $4066
+
+logo_ad:
+
+	DEFB	$00,$00,$02,$00,$00,$02,$00,$00
+	DEFB	$06,$00,$00,$06,$00,$00,$06,$00
+	DEFB	$00,$06,$00,$00,$0E,$00,$00,$0C
+	DEFB	$78,$00,$0C,$F8,$00,$0C,$D8,$03
+	DEFB	$FC,$18,$1F,$F8,$18,$7F,$F8,$30
+	DEFB	$F0,$79,$30,$E0,$33,$E1,$C0,$3F
+	DEFB	$E1,$C0,$3C,$E1,$C0,$70,$79,$E0
+	DEFB	$70,$78,$FF,$E0,$F8,$3F,$C0,$F0
+	DEFB	$3E,$00,$00,$00,$00,$00,$00,$00
+
+;	Pixel Size:      ( 24,  16)
+;
+;	(Puntero_de_impresion) ..... $4089
+
+logo_eu:
+
+	DEFB	$00,$F0,$00,$03,$F8,$18,$07,$FC
+	DEFB	$38,$1E,$0C,$30,$3C,$1C,$70,$7C
+	DEFB	$F8,$60,$7F,$F0,$E0,$7F,$C0,$C0
+	DEFB	$78,$00,$C1,$70,$1C,$C3,$70,$7C
+	DEFB	$C7,$7F,$F0,$FF,$1F,$E0,$7C,$0F
+	DEFB	$00,$38,$00,$00,$00,$00,$00,$00
+
+;	Pixel Size:      ( 24,  16)
+;
+;	(Puntero_de_impresion) ..... $408c
+
+
+logo_us:
+
+	DEFB	$00,$00,$00,$00,$1E,$00,$00,$3F
+	DEFB	$00,$00,$73,$80,$30,$E1,$C0,$70
+	DEFB	$E1,$C0,$60,$70,$C0,$C0,$30,$00
+	DEFB	$C0,$38,$00,$80,$1C,$00,$80,$0F
+	DEFB	$00,$8F,$C3,$80,$E7,$FF,$80,$E0
+	DEFB	$3F,$00,$00,$00,$00,$00,$00,$00
+
+;	---------- + ----------
+
+;	Disparo.
 
 Disparo_de_entidad_derecho DEFB $18,$00,$00
 Disparo_de_entidad_izquierdo DEFB $00,$00,$18
@@ -16,22 +97,29 @@ Indice_disparo_Amadeus defw Disparo_0
 	defw Disparo_fb
 	defw Disparo_fd
 
-; Disparo (CTRL_DESPLZ)="0".
+;	Disparo (CTRL_DESPLZ)="0".
+
 Disparo_0 DEFB $01,$80
 Disparo_0b DEFB $07,$e0 						; (No se imprime, detección de colisión).
-; Disparo (CTRL_DESPLZ)="f9"
+
+;	Disparo (CTRL_DESPLZ)="f9"
+
 Disparo_f9 DEFB $00,$60
 Disparo_f9b DEFB $01,$f8 						; (No se imprime, detección de colisión).
-; Disparo (CTRL_DESPLZ)="fb" 
+
+;	Disparo (CTRL_DESPLZ)="fb"
+
 Disparo_fb DEFB $18,$00
 Disparo_fbb DEFB $7e,$00 						; (No se imprime, detección de colisión).
-; Disparo (CTRL_DESPLZ)="fd" 
+
+;	Disparo (CTRL_DESPLZ)="fd"
+
 Disparo_fd DEFB $06,$00
 Disparo_fdb DEFB $1f,$80 						; (No se imprime, detección de colisión).
 
 ; ----------------------------------------------------------------------------------------
 
-; Badplate.
+;	Badplate.
 
 Indice_Badplate_der defw Badplate
 	defw Badplate_f8								
@@ -127,7 +215,7 @@ Badplate_fe
 	DEFB	$3C,$01,$B8,$76,$01,$8F,$C6,$00
 	DEFB	$C7,$8C,$00,$77,$B8,$00,$1F,$E0
 
-; Badsat.
+;	Badsat.
 
 Indice_Badsat_izq defw Badsat_izquierda
 	defw Badsat_izq_fe
@@ -153,56 +241,56 @@ Badsat_izquierda DEFB $00,$08,$00
 	DEFB	$AA,$60,$00
 	DEFB 	$54,$00,$00
 	DEFB 	$28,$00,$00
-	DEFB 	$10,$00,$00	 ; Sprite principal a izquierda, (sin desplazar).
+	DEFB 	$10,$00,$00	 						; Sprite principal a izquierda, (sin desplazar).
 
 Badsat_izq_f8 DEFB $00,$04,$00,$01,$0A,$00,$01,$15
 	DEFB	$00,$01,$2A,$80,$01,$55,$00,$01
 	DEFB	$2A,$00,$3F,$F4,$00,$01,$B0,$00
 	DEFB	$05,$70,$00,$0B,$FC,$00,$15,$F4
 	DEFB	$00,$2A,$20,$00,$55,$30,$00,$2A
-	DEFB	$00,$00,$14,$00,$00,$08,$00,$00 ; $F8 (7º DESPLZ a izquierda).
+	DEFB	$00,$00,$14,$00,$00,$08,$00,$00 	; $F8 (7º DESPLZ a izquierda).
 
 Badsat_izq_f9 DEFB $00,$02,$00,$00,$85,$00,$00,$8A
 	DEFB	$80,$00,$95,$40,$00,$AA,$80,$00
 	DEFB	$95,$00,$1F,$FA,$00,$00,$D8,$00
 	DEFB	$02,$B8,$00,$05,$FE,$00,$0A,$FA
 	DEFB	$00,$15,$10,$00,$2A,$98,$00,$15
-	DEFB	$00,$00,$0A,$00,$00,$04,$00,$00 ; $F9 (6º DESPLZ a izquierda).
+	DEFB	$00,$00,$0A,$00,$00,$04,$00,$00 	; $F9 (6º DESPLZ a izquierda).
 
 Badsat_izq_fa DEFB $00,$01,$00,$00,$42,$80,$00,$45
 	DEFB	$40,$00,$4A,$A0,$00,$55,$40,$00
 	DEFB	$4A,$80,$0F,$FD,$00,$00,$6C,$00
 	DEFB	$01,$5C,$00,$02,$FF,$00,$05,$7D
 	DEFB	$00,$0A,$88,$00,$15,$4C,$00,$0A
-	DEFB	$80,$00,$05,$00,$00,$02,$00,$00 ; $Fa (5º DESPLZ a izquierda).
+	DEFB	$80,$00,$05,$00,$00,$02,$00,$00 	; $Fa (5º DESPLZ a izquierda).
 
 Badsat_izq_fb DEFB $00,$00,$80,$00,$21,$40,$00,$22
 	DEFB	$A0,$00,$25,$50,$00,$2A,$A0,$00
 	DEFB	$25,$40,$07,$FE,$80,$00,$36,$00
 	DEFB	$00,$AE,$00,$01,$7F,$80,$02,$BE
 	DEFB	$80,$05,$44,$00,$0A,$A6,$00,$05
-	DEFB	$40,$00,$02,$80,$00,$01,$00,$00 ; $Fb (4º DESPLZ a izquierda).
+	DEFB	$40,$00,$02,$80,$00,$01,$00,$00 	; $Fb (4º DESPLZ a izquierda).
 
 Badsat_izq_fc DEFB $00,$00,$40,$00,$10,$A0,$00,$11
 	DEFB	$50,$00,$12,$A8,$00,$15,$50,$00
 	DEFB	$12,$A0,$03,$FF,$40,$00,$1B,$00
 	DEFB	$00,$57,$00,$00,$BF,$C0,$01,$5F
 	DEFB	$40,$02,$A2,$00,$05,$53,$00,$02
-	DEFB	$A0,$00,$01,$40,$00,$00,$80,$00 ; $Fc (3er DESPLZ a izquierda).
+	DEFB	$A0,$00,$01,$40,$00,$00,$80,$00 	; $Fc (3er DESPLZ a izquierda).
 
 Badsat_izq_fd DEFB $00,$00,$20,$00,$08,$50,$00,$08
 	DEFB	$A8,$00,$09,$54,$00,$0A,$A8,$00
 	DEFB	$09,$50,$01,$FF,$A0,$00,$0D,$80
 	DEFB	$00,$2B,$80,$00,$5F,$F8,$00,$AF
 	DEFB	$80,$01,$51,$00,$02,$A9,$00,$01
-	DEFB	$51,$00,$00,$A0,$00,$00,$40,$00 ; $Fd (2º DESPLZ a izquierda).
+	DEFB	$51,$00,$00,$A0,$00,$00,$40,$00 	; $Fd (2º DESPLZ a izquierda).
 
 Badsat_izq_fe DEFB $00,$00,$10,$00,$04,$28,$00,$04
 	DEFB	$54,$00,$04,$AA,$00,$05,$54,$00
 	DEFB	$04,$A8,$00,$FF,$D0,$00,$06,$C0
 	DEFB	$00,$15,$C0,$00,$2F,$FC,$00,$57
 	DEFB	$C0,$00,$A8,$80,$01,$54,$80,$00
-	DEFB	$A8,$80,$00,$50,$00,$00,$20,$00 ; $Fe (1er DESPLZ a izquierda).
+	DEFB	$A8,$80,$00,$50,$00,$00,$20,$00 	; $Fe (1er DESPLZ a izquierda).
 
 
 Indice_Badsat_der defw Badsat_derecha
@@ -229,42 +317,42 @@ Badsat_derecha DEFB	$10,$00,$00
 	DEFB	$06,$55,$00
 	DEFB	$00,$2A,$00
 	DEFB	$00,$14,$00
-	DEFB	$00,$08,$00 ; Sprite principal a derecha, (sin desplazar).
+	DEFB	$00,$08,$00 						; Sprite principal a derecha, (sin desplazar).
 
 Badsat_der_f8 DEFB $08,$00,$00,$14,$20,$00,$2A,$20
 	DEFB	$00,$55,$20,$00,$2A,$A0,$00,$15
 	DEFB	$20,$00,$0B,$FF,$00,$03,$60,$00
 	DEFB	$03,$A8,$00,$0F,$F4,$00,$0B,$EA
 	DEFB	$00,$01,$15,$00,$03,$2A,$80,$00
-	DEFB	$15,$00,$00,$0A,$00,$00,$04,$00 ; $F8 (1er DESPLZ a derecha).
+	DEFB	$15,$00,$00,$0A,$00,$00,$04,$00 	; $F8 (1er DESPLZ a derecha).
 
 Badsat_der_f9 DEFB $04,$00,$00,$0A,$10,$00,$15,$10
 	DEFB	$00,$2A,$90,$00,$15,$50,$00,$0A
 	DEFB	$90,$00,$05,$FF,$80,$01,$B0,$00
 	DEFB	$01,$D4,$00,$07,$FA,$00,$05,$F5
 	DEFB	$00,$00,$8A,$80,$01,$95,$40,$00
-	DEFB	$0A,$80,$00,$05,$00,$00,$02,$00 ; $F9 (2º DESPLZ a derecha).
+	DEFB	$0A,$80,$00,$05,$00,$00,$02,$00 	; $F9 (2º DESPLZ a derecha).
 
 Badsat_der_fa DEFB $02,$00,$00,$05,$08,$00,$0A,$88
 	DEFB	$00,$15,$48,$00,$0A,$A8,$00,$05
 	DEFB	$48,$00,$02,$FF,$C0,$00,$D8,$00
 	DEFB	$00,$EA,$00,$03,$FD,$00,$02,$FA
 	DEFB	$80,$00,$45,$40,$00,$CA,$A0,$00
-	DEFB	$05,$40,$00,$02,$80,$00,$01,$00 ; $Fa (3er DESPLZ a derecha).
+	DEFB	$05,$40,$00,$02,$80,$00,$01,$00 	; $Fa (3er DESPLZ a derecha).
 
 Badsat_der_fb DEFB $01,$00,$00,$02,$84,$00,$05,$44
 	DEFB	$00,$0A,$A4,$00,$05,$54,$00,$02
 	DEFB	$A4,$00,$01,$7F,$E0,$00,$6C,$00
 	DEFB	$00,$75,$00,$01,$FE,$80,$01,$7D
 	DEFB	$40,$00,$22,$A0,$00,$65,$50,$00
-	DEFB	$02,$A0,$00,$01,$40,$00,$00,$80 ; $Fb (4º DESPLZ a derecha).
+	DEFB	$02,$A0,$00,$01,$40,$00,$00,$80 	; $Fb (4º DESPLZ a derecha).
 
 Badsat_der_fc DEFB $00,$80,$00,$01,$42,$00,$02,$A2
 	DEFB	$00,$05,$52,$00,$02,$AA,$00,$01
 	DEFB	$52,$00,$00,$BF,$F0,$00,$36,$00
 	DEFB	$00,$3A,$80,$00,$FF,$40,$00,$BE
 	DEFB	$A0,$00,$11,$50,$00,$32,$A8,$00
-	DEFB	$01,$50,$00,$00,$A0,$00,$00,$40 ; $Fc (5º DESPLZ a derecha).
+	DEFB	$01,$50,$00,$00,$A0,$00,$00,$40 	; $Fc (5º DESPLZ a derecha).
 
 	org $8800
 
@@ -273,36 +361,36 @@ Badsat_der_fd DEFB $00,$40,$00,$00,$A1,$00,$01,$51
 	DEFB	$A9,$00,$00,$5F,$F8,$00,$1B,$00
 	DEFB	$00,$1D,$40,$01,$FF,$A0,$00,$1F
 	DEFB	$50,$00,$08,$A8,$00,$09,$54,$00
-	DEFB	$08,$A8,$00,$00,$50,$00,$00,$20 ; $Fd (6º DESPLZ a derecha).
+	DEFB	$08,$A8,$00,$00,$50,$00,$00,$20 	; $Fd (6º DESPLZ a derecha).
 
 Badsat_der_fe DEFB $00,$20,$00,$00,$50,$80,$00,$A8
 	DEFB	$80,$01,$54,$80,$00,$AA,$80,$00
 	DEFB	$54,$80,$00,$2F,$FC,$00,$0D,$80
 	DEFB	$00,$0E,$A0,$00,$FF,$D0,$00,$0F
 	DEFB	$A8,$00,$04,$54,$00,$04,$AA,$00
-	DEFB	$04,$54,$00,$00,$28,$00,$00,$10 ; $Fe (7º DESPLZ a derecha).
+	DEFB	$04,$54,$00,$00,$28,$00,$00,$10 	; $Fe (7º DESPLZ a derecha).
 
 ; ----------------------------------------------------------------------------------------
 
-; Amadeus. 2x2.
+; 	Amadeus. 2x2.
 
 Indice_Amadeus_der defw Amadeus
 	defw 0	
-	defw Amadeus_F9							; [$F9] right - [$FA] left 
+	defw Amadeus_F9								; [$F9] right - [$FA] left
 	defw 0	
-	defw Amadeus_Fb     					; [$FB] right - [$FC] left                     
+	defw Amadeus_Fb     						; [$FB] right - [$FC] left
 	defw 0	
-	defw Amadeus_Fd							; [$FD] right - [$FE] left 
-	defw 0	 								; (Fín de índice).
+	defw Amadeus_Fd								; [$FD] right - [$FE] left
+	defw 0	 									; (Fín de índice).
 
 Indice_Amadeus_izq defw Amadeus
 	defw 0	
-	defw Amadeus_Fd							; [$F9] right - [$FA] left 
+	defw Amadeus_Fd								; [$F9] right - [$FA] left
 	defw 0	
-	defw Amadeus_Fb     					; [$FB] right - [$FC] left                     
+	defw Amadeus_Fb     						; [$FB] right - [$FC] left
 	defw 0	
-	defw Amadeus_F9							; [$FD] right - [$FE] left 
-	defw 0	 								; (Fín de índice).
+	defw Amadeus_F9								; [$FD] right - [$FE] left
+	defw 0	 									; (Fín de índice).
 
 ;	org $8700
 
@@ -321,14 +409,14 @@ Amadeus DEFB $01,$80,0
 	DEFB	$FB,$DF,0
 	DEFB	$FB,$DF,0
 	DEFB	$8B,$D1,0
-	DEFB	$71,$8E,0 						; Sprite principal, (sin desplazar).
+	DEFB	$71,$8E,0 							; Sprite principal, (sin desplazar).
 
 Amadeus_F9 DEFB	$00,$60,$00,$08,$F1,$00,$09,$99
 	DEFB	$00,$09,$09,$00,$0B,$0D,$00,$1B
 	DEFB	$6D,$80,$1B,$FD,$80,$19,$F9,$80
 	DEFB	$39,$F9,$C0,$3C,$F3,$C0,$3D,$FB
 	DEFB	$C0,$3F,$FF,$C0,$3E,$F7,$C0,$3E
-	DEFB	$F7,$C0,$22,$F4,$40,$1C,$63,$80 ; $F9 (2º DESPLZ a derecha).
+	DEFB	$F7,$C0,$22,$F4,$40,$1C,$63,$80 	; $F9 (2º DESPLZ a derecha).
 
 	org $8900
 
@@ -337,14 +425,14 @@ Amadeus_Fb DEFB	$00,$18,$00,$02,$3C,$40,$02,$66
 	DEFB	$DB,$60,$06,$FF,$60,$06,$7E,$60
 	DEFB	$0E,$7E,$70,$0F,$3C,$F0,$0F,$7E
 	DEFB	$F0,$0F,$FF,$F0,$0F,$BD,$F0,$0F
-	DEFB	$BD,$F0,$08,$BD,$10,$07,$18,$E0 ; $Fb (4º DESPLZ a derecha).
+	DEFB	$BD,$F0,$08,$BD,$10,$07,$18,$E0 	; $Fb (4º DESPLZ a derecha).
 
 Amadeus_Fd DEFB	$00,$06,$00,$00,$8F,$10,$00,$99
 	DEFB	$90,$00,$90,$90,$00,$B0,$D0,$01
 	DEFB	$B6,$D8,$01,$BF,$D8,$01,$9F,$98
 	DEFB	$03,$9F,$9C,$03,$CF,$3C,$03,$DF
 	DEFB	$BC,$03,$FF,$FC,$03,$EF,$7C,$03
-	DEFB	$EF,$7C,$02,$2F,$44,$01,$C6,$38 ; $Fd (6º DESPLZ a derecha).
+	DEFB	$EF,$7C,$02,$2F,$44,$01,$C6,$38 	; $Fd (6º DESPLZ a derecha).
 
 ; ----------------------------------------------------------------------------------------
 
