6423a35a415ee2249e73e4ff177d0ab3a1f0a1f7
paco_lorente <lo-rente@hotmail.com>
2026-03-23 21:31:02 +0100

    23/3/26. Mensajes de nivel, estrategia para borrarlos.

--- Banco_de_pruebas.asm
+++ Banco_de_pruebas.asm
@@ -822,7 +822,7 @@ Puntero_indice_de_almacenes defw Almacen_de_movimientos_masticados_1
 																				
 Puntero_de_entidades defw 0									; Este puntero se va desplazando por los distintos bytes_
 ; 															; _ que definen el NIVEL.
-Puntero_de_mensajes_de_niveles defw 0 ;Msg_level_index 		; Inicialmente apunta al mensaje del nivel 1.
+Puntero_de_mensajes_de_niveles defw Msg_level_index 		; Inicialmente apunta al mensaje del nivel 1.
 
 ; ---------------------------------------------------------------------------------------------------------------
 
